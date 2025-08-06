4d20ffd9205c63a5c6bc71884c05654d748a7b94
fernando suaiden <fersuaiden@hotmail.com>
2025-08-06 19:41:42 -0300

    refactor: Migrate to ES Modules and secure credentials with .env

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import bd from './db.js'
 
 const app = express();
 const PORT = 3001;
@@ -11,6 +13,17 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Servidor da API de E-commerce está no ar!');
 });
+
+// Rota para buscar todos os produtos
+app.get('/api/produtos', async (req, res) => {
+    try{
+        const [produtos] = await bd.query('SELECT * FROM produtos');
+        res.status(200).json(produtos);
+    }catch(error){
+        console.error('Erro ao buscar produtos:', error);
+        res.status(500).json({message: 'Erro interno do servidor ao buscar produtos.'})
+    }
+})
 
 app.listen(PORT, ()=>{
     console.log(`Servidor rodando na porta ${PORT}`);
